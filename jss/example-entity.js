adbbd5845420e4a2af3dd856c20ad51184753a01
sumitrag-david <davisxdpfr@gmail.com>
2016-07-12 11:59:58 -0500

    adding file with json data

---
+++ jss/example-entity.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sumitrag-david on 12/07/16.
+ */
+
+var example = {
+  'verbosePlural': 'Examples',
+  'verbose': 'Example',
+  'entityName':'example',
+  'optional': false,
+  'table_headers': ['Name', 'Phone', 'Address'],
+  'actionFields': ['Edit','Delete'],
+  'table':true,
+  'tableActions': false,
+};
---
+++ jss/operations.js
@@ -0,0 +1,24 @@
+function decodeUNICODE(string) {
+  'use strict';
+  string = decodeURIComponent(string);
+  var r = /\\u([\d\w]{4})/gi;
+  string = string.replace(r, function(match, grp) {
+    return String.fromCharCode(parseInt(grp, 16));
+  });
+  string = decodeURI(string);
+
+  return string;
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function updateStyleDOM(scope, variable, value) {
+  scope.customStyle[variable] = value;
+  scope.updateStyles();
+}
+
+
+
+
