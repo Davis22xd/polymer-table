81144b9b2d596bfb19e47a387a7a062e2abfae15
sumitrag-david <davisxdpfr@gmail.com>
2016-07-14 09:55:52 -0500

    adding sorting column behavior

--- jss/operations.js
+++ jss/operations.js
@@ -1,14 +1,3 @@
-function decodeUNICODE(string) {
-  'use strict';
-  string = decodeURIComponent(string);
-  var r = /\\u([\d\w]{4})/gi;
-  string = string.replace(r, function(match, grp) {
-    return String.fromCharCode(parseInt(grp, 16));
-  });
-  string = decodeURI(string);
-
-  return string;
-}
 
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -19,12 +8,6 @@ function updateStyleDOM(scope, variable, value) {
   scope.updateStyles();
 }
 
-//
-//function init() {
-//  var sorter = tsorter.create('TABLE-ID');
-//}
-//
-//window.onload = init;
 
 
 
